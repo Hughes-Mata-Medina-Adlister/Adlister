64cc49a4b247c07dfc49576476c0c0022a60b584
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-08-01 12:23:07 -0500

    Adjusted DeleteAd and SearchedAd

--- src/main/java/com/codeup/adlister/controllers/DeleteAd.java
+++ src/main/java/com/codeup/adlister/controllers/DeleteAd.java
@@ -9,16 +9,12 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
 public class DeleteAd extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         String id = request.getParameter("id");
-
         try {
             Class.forName("com.mysql.jdbc.Driver");
             String user = "root";
@@ -27,13 +23,10 @@ public class DeleteAd extends HttpServlet {
             Connection con = DriverManager.getConnection("jdbc:mysql://locahost:8080/ads", user, pass);
             PreparedStatement ps = con.prepareStatement("delete from adlister_db where id=?");
             ps.setString(1, id);
-
             int i = ps.executeUpdate();
-
             if (i > 0) {
                 out.println("Ad successfully removed...");
             }
-
         } catch (Exception e) {
             System.out.println(e);
         }
